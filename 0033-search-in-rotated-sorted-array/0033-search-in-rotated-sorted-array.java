a186eb1aa3d505278b86fa5791c645bb9b7614d3
Goutham Kumar <kgoutham2k5@gmail.com>
2026-04-02 20:51:52 +0530

    Time: 0 ms (100%), Space: 43.9 MB (55.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,43 +1,31 @@
 class Solution {
     public int search(int[] arr, int target) {
-        int length=arr.length;
-        int low=0;
-        int high=length-1;
-        while(low<=high){
-            int mid=(low+high)/2;
+        int left=0;
+        int right=arr.length-1;
+        while(left<=right){
+            int mid=left+ (right-left)/2;
             if(arr[mid]==target){
                 return mid;
             }
-            if(arr[low]<=arr[mid]){//left sorted
-                if(arr[low]<=target && target<=arr[mid]){
-                    high=mid-1;
+            if(arr[left]<=arr[mid]){
+                if(arr[left]<=target && target<=arr[mid]){
+                    right=mid-1;
                 }
                 else{
-                    low=mid+1;
+                    left=mid+1;
                 }
             }
-            else if(arr[mid]<=arr[high]){
-                if(arr[mid]<=target && target<=arr[high]){
-                    low=mid+1;//right sorted
+            if(arr[right]>=arr[mid]){
+                if(arr[mid]<=target && target<=arr[right]){
+                    left=mid+1;
                 }
                 else{
-                    high=mid-1;
+                    right=mid-1;
                 }
             }
+            
         }
         return -1;
-    }
-
-    public  void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        int target=sc.nextInt();
-        int result=search(arr,target);
-        System.out.println(result);
     }
 
 }
