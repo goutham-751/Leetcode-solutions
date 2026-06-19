786077af78f266818700f76362e470b8a12e3bad
Goutham Kumar <kgoutham2k5@gmail.com>
2026-06-19 09:50:59 +0530

    Time: 0 ms (100%), Space: 43 MB (55.78%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -3,10 +3,13 @@ class Solution {
         int n=gain.length;
         int[] prefix=new int[n+1];
         prefix[0]=0;
-        for(int i=0;i<n;i++){
-            prefix[i+1]=prefix[i]+gain[i];
+        for(int i=1;i<=n;i++){
+            prefix[i]=prefix[i-1]+gain[i-1];
         }
-        Arrays.sort(prefix);
-        return prefix[n];
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n+1;i++){
+            max=Math.max(prefix[i],max);
+        }
+        return max;
     }
 }
