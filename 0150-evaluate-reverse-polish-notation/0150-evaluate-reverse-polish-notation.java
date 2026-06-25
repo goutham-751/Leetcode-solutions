41ff76426a50dee05c29aa3d06f2a4076eba9404
Goutham Kumar <kgoutham2k5@gmail.com>
2026-06-25 17:37:46 +0530

    Time: 7 ms (35.99%), Space: 45.3 MB (63.03%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,24 +1,26 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> st=new Stack<>();
-        for(String token:tokens){
-            if(isoperator(token)){
+        for(String c:tokens){
+
+            if(isoperator(c) && !st.isEmpty()){
                 int a=st.pop();
                 int b=st.pop();
-                switch(token){
-                    case "+": st.push(a+b); break;
-                    case "-": st.push(b-a);break;
-                    case "/":st.push(b/a);break;
+                switch(c){
+                    case "+":st.push(a+b);break;
+                    case "-":st.push(b-a);break;
                     case "*":st.push(a*b);break;
-                }    
+                    case "/":st.push(b/a);break;
+                }
             }
-            else{
-                st.push(Integer.parseInt(token));
+            else{    
+                st.push(Integer.parseInt(c));
             }
         }
-        return st.peek();//this will be your final result
+        return st.pop();
+
     }
-    public boolean isoperator(String token){
-        return token.equals("+") ||  token.equals("*") ||  token.equals("-") ||  token.equals("/");
+    public boolean isoperator(String c){
+        return c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/");
     }
 }
