c1f8dd2c45c9e32f7d5317c1aac8a4b90b43fa83
Goutham Kumar <kgoutham2k5@gmail.com>
2026-09-14 21:56:38 +0530

    Time: 4 ms (2.98%), Space: 45.5 MB (62.56%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,27 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result=new ArrayList<>();
-        solve(nums,new boolean[nums.length],new ArrayList<>(),result);
+        boolean[] visited=new  boolean[nums.length];
+        Arrays.sort(nums);
+        solve(result,new ArrayList<>(),nums,visited);
         return result;
     }
-    public void solve(int[] nums,boolean[] visited,List<Integer>current,List<List<Integer>> result){
-        //base condition 
-        if(nums.length==current.size()){
+    public void solve(List<List<Integer>> result,List<Integer> current,int[] nums,boolean[] visited){
+        if(current.size()==nums.length){
             result.add(new ArrayList<>(current));
             return;
         }
         for(int i=0;i<nums.length;i++){
-            if(visited[i]) continue;
+            if(visited[i]) {
+                continue;
+            }
             visited[i]=true;
-            current.add(nums[i]);
-            solve(nums,visited,current,result);
-            current.remove(current.size()-1);
+            current.add(nums[i]);//make the choice 
+            solve(result,current,nums,visited);
+            current.remove(current.size()-1);//backtrack
             visited[i]=false;
         }
+
 
     }
 }
